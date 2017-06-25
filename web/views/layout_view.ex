40b4bcf55584bc219aea0194620d3bfd11e95ee2
Dmitry Stropalov <d.stropalov@gmail.com>
2017-06-25 11:42:22 +0200

    Expose manifest.json in two ways.

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,18 @@
 defmodule Userlist.LayoutView do
   use Userlist.Web, :view
+
+  def digest do
+      manifest =
+        Application.get_env(:userlist, Userlist.Endpoint, %{})[:cache_static_manifest]
+        || "priv/static/manifest.json"
+
+      manifest_file = Application.app_dir(:userlist, manifest)
+
+      if File.exists?(manifest_file) do
+        manifest_file
+        |> File.read!
+      else
+        %{}
+      end
+    end
 end
